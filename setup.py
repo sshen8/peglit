5849e3bf4a9f84a6858951d0953d0b55d07d7bf5
Simon Shen <sshen8@gmail.com>
2023-01-27 20:19:06 -0500

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="peglit",
-    version="1.0.1",
+    version="1.0.2",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://www.github.com/sshen8/peglit",
